665b343a2ec1f2f024444a13fcb00f20fd6d95f1
Andrew <andrew.orodenker@gmail.com>
2023-04-28 15:14:56 -0700

    40-42-r3f complete

--- 40-42-r3f/src/App.jsx
+++ 40-42-r3f/src/App.jsx
@@ -1,12 +1,48 @@
-import { Canvas } from '@react-three/fiber';
+import { useFrame, extend, useThree } from '@react-three/fiber';
+import { useRef } from 'react';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import CustomObject from './components/CustomObject';
+
+extend({ OrbitControls });
 
 const App = () => {
+
+  const { camera, gl } = useThree();
+  const cubeRef = useRef();
+  const groupRef = useRef();
+
+  useFrame((state, delta) => {
+    // const angle = state.clock.elapsedTime;
+    // state.camera.position.x = Math.sin(angle) * 8;
+    // state.camera.position.z = Math.cos(angle) * 8;
+    // state.camera.lookAt(0, 0, 0);
+    cubeRef.current.rotation.y += delta;
+  });
+
   return(
     <>
-      <mesh>
-        <torusKnotGeometry />
-        <meshNormalMaterial />
+      <orbitControls args={[ camera, gl.domElement ]} />
+      <directionalLight position={[ 1, 2, 3 ]} intensity={ 1.5 } />
+      <ambientLight intensity={ 0.5 } />
+
+      <group ref={groupRef} >
+        <mesh position-x={ -2 } >
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
+        </mesh>
+
+        <mesh ref={cubeRef} scale={ 1.5 } position-x={ 2 } rotation-y={ Math.PI * 0.25 } >
+          <boxGeometry />
+          <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+      </group>
+
+      <mesh scale={ 10 } rotation-x={ -Math.PI * 0.5 } position-y={ -1 } >
+        <planeGeometry />
+        <meshStandardMaterial color="green" />
       </mesh>
+
+      <CustomObject />
     </>
   );
 };
--- 40-42-r3f/src/index.jsx
+++ 40-42-r3f/src/index.jsx
@@ -2,11 +2,26 @@ import './style.css';
 import ReactDOM from 'react-dom/client';
 import { Canvas } from '@react-three/fiber';
 import App from './App';
+import { ACESFilmicToneMapping, sRGBEncoding } from 'three';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const cameraSettings = {
+    fov: 45,
+    near: 0.1,
+    far: 200,
+    position: [3, 2, 6]
+};
+
 root.render(
-    <Canvas>
+    <Canvas
+        flat
+        gl={{
+            toneMapping: ACESFilmicToneMapping,
+            outputEncoding: sRGBEncoding
+        }}
+        camera={ cameraSettings }
+    >
         <App />
     </Canvas>
 );
